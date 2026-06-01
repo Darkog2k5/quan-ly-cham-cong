0706c0734355fcaee3fe5f518bb1b339da67b059
khoi-nguyen-005 <n23dccn032@student.ptithcm.edu.vn>
2026-06-02 00:47:22 +0700

    Add files via upload

---
+++ src/main/java/vn/edu/project/model/CaLamViec.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author VICTUS
+ */
+package vn.edu.project.model;
+
+import java.sql.Time;
+
+public class CaLamViec {
+    private String maCa;
+    private String tenCa;
+    private Time gioBatDau;
+    private Time gioKetThuc;
+
+    public CaLamViec() {}
+
+    public CaLamViec(String maCa, String tenCa, Time gioBatDau, Time gioKetThuc) {
+        this.maCa = maCa;
+        this.tenCa = tenCa;
+        this.gioBatDau = gioBatDau;
+        this.gioKetThuc = gioKetThuc;
+    }
+
+    public String getMaCa() { return maCa; }
+    public void setMaCa(String maCa) { this.maCa = maCa; }
+
+    public String getTenCa() { return tenCa; }
+    public void setTenCa(String tenCa) { this.tenCa = tenCa; }
+
+    public Time getGioBatDau() { return gioBatDau; }
+    public void setGioBatDau(Time gioBatDau) { this.gioBatDau = gioBatDau; }
+
+    public Time getGioKetThuc() { return gioKetThuc; }
+    public void setGioKetThuc(Time gioKetThuc) { this.gioKetThuc = gioKetThuc; }
+
+    @Override
+    public String toString() {
+        return tenCa;
+    }
+}
---
+++ src/main/java/vn/edu/project/model/ChamCong.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vn.edu.project.model;
+
+/**
+ *
+ * @author VICTUS
+ */
+
+
+import java.sql.Timestamp;
+
+public class ChamCong {
+    private String maCong;
+    private String maLich;
+    private Timestamp gioVao;
+    private Timestamp gioRa;
+    private String trangThai;
+    private String minhChung;
+
+    public ChamCong() {}
+
+    public ChamCong(String maCong, String maLich, Timestamp gioVao, Timestamp gioRa, String trangThai, String minhChung) {
+        this.maCong = maCong;
+        this.maLich = maLich;
+        this.gioVao = gioVao;
+        this.gioRa = gioRa;
+        this.trangThai = trangThai;
+        this.minhChung = minhChung;
+    }
+
+    public String getMaCong() { return maCong; }
+    public void setMaCong(String maCong) { this.maCong = maCong; }
+
+    public String getMaLich() { return maLich; }
+    public void setMaLich(String maLich) { this.maLich = maLich; }
+
+    public Timestamp getGioVao() { return gioVao; }
+    public void setGioVao(Timestamp gioVao) { this.gioVao = gioVao; }
+
+    public Timestamp getGioRa() { return gioRa; }
+    public void setGioRa(Timestamp gioRa) { this.gioRa = gioRa; }
+
+    public String getTrangThai() { return trangThai; }
+    public void setTrangThai(String trangThai) { this.trangThai = trangThai; }
+
+    public String getMinhChung() { return minhChung; }
+    public void setMinhChung(String minhChung) { this.minhChung = minhChung; }
+}
